512ce60b31a52ea067ea62654a0c8c1ef5b601a6
unknown <maldiniradjab@gmail.com>
2024-10-01 13:15:49 +0700

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import classes from "./Header.module.css";
+import { authActions } from "../store/auth";
+
+const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
+
+  return (
+    <header className={classes.header}>
+      <h1>Redux Auth</h1>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
+  );
+};
+
+export default Header;
